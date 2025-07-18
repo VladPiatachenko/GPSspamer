712e6f3ddb0b146b29a8842f87800b32518512a6
Vlad Piatachenko <vlpiatachenko@gmail.com>
2025-07-18 14:08:04 +0300

    Basic GPS coordinate generation

--- src/main/java/sumdu/edu/ua/GPSspamer/GpSspamerApplication.java
+++ src/main/java/sumdu/edu/ua/GPSspamer/GpSspamerApplication.java
@@ -2,12 +2,20 @@ package sumdu.edu.ua.GPSspamer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class GpSspamerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GpSspamerApplication.class, args);
-	}
+		public static void main(String[] args) {
+			SpringApplication.run(GpSspamerApplication.class, args);
+		}
 
+		@Bean
+		public GpsGenerator gpsGenerator() {
+			return new GpsGenerator();
+		}
 }
+
+
